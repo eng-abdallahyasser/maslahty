dcc83e989f6411a935bc5edba6dc53496b064b26
MohsenAbdo22 <mohsenabdo213@gmail.com>
2026-04-09 23:01:51 +0200

    Add use cases and data models for transfer requests and price warnings

---
+++ app/src/main/java/com/abdallahyasser/maslahty/data/models/PriceWarningDto.kt
@@ -0,0 +1,17 @@
+package com.abdallahyasser.maslahty.data.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PriceWarningDto(
+    @SerialName("market_price")
+    val marketPrice: Double,
+    @SerialName("difference")
+    val difference: Double,
+    @SerialName("percentage")
+    val percentage: Double,
+    @SerialName("message")
+    val message: String
+
+)
