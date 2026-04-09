6d888414b1aa0884859d56543102c9de0dab2243
MohsenAbdo22 <mohsenabdo213@gmail.com>
2026-04-09 17:25:03 +0200

    Add navigation routes and update dependencies for splash screen and serialization

--- app/src/main/java/com/abdallahyasser/maslahty/presentaion/navigation/NavGraph.kt
+++ app/src/main/java/com/abdallahyasser/maslahty/presentaion/navigation/NavGraph.kt
@@ -2,10 +2,9 @@ package com.abdallahyasser.maslahty.presentaion.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.abdallahyasser.maslahty.presentaion.screens.auth.LoginScreen
 import com.abdallahyasser.maslahty.presentaion.screens.auth.OTPVerificationScreen
 import com.abdallahyasser.maslahty.presentaion.screens.auth.RegistrationScreen
@@ -18,52 +17,45 @@ import com.abdallahyasser.maslahty.presentaion.screens.splash.SplashScreen
 fun NavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.SplashScreen.route
-    )
-
-
-    {
-        composable(Screen.SplashScreen.route) {
-                SplashScreen(navController = navController)
+        startDestination = Route.Splash // بنمرر الـ Object نفسه
+    ) {
+        composable<Route.Splash> {
+            SplashScreen(navController = navController)
         }
-        composable(Screen.LoginScreen.route) {
+
+        composable<Route.Login> {
             LoginScreen(navController = navController)
         }
-            composable(
-                Screen.OTP.route,
-                arguments = listOf(navArgument("phoneNumber")
-                { type = NavType.StringType }
-                )
-                ) { backStackEntry ->
-                val phoneNumber = backStackEntry.arguments?.getString("phoneNumber").orEmpty()
-                OTPVerificationScreen(
-                    navController = navController,
-                    phoneNumber = phoneNumber
-                )
-            }
-        composable (Screen.RegistrationScreen.route)
-        {
-          RegistrationScreen(navController=navController )
+
+        // هنا بنستخدم toRoute لفك البيانات تلقائياً
+        composable<Route.OTP> { backStackEntry ->
+            val args = backStackEntry.toRoute<Route.OTP>()
+            OTPVerificationScreen(
+                navController = navController,
+                phoneNumber = args.phoneNumber
+            )
         }
-        composable (Screen.HomeScreen.route){
-            HomeScreen(navController=navController)
+
+        composable<Route.Registration> {
+            RegistrationScreen(navController = navController)
         }
-        composable(Screen.VehicleDetailsScreen.route) {
+
+        composable<Route.Home> {
+            HomeScreen(navController = navController)
+        }
+
+        composable<Route.VehicleDetails> {
             VehicleDetailsScreen(navController = navController)
         }
 
-        composable(
-            route = Screen.ImageUploadScreen.route,
-            arguments = listOf(navArgument("vehicleId") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val vehicleId = backStackEntry.arguments?.getString("vehicleId").orEmpty()
-            ImageUploadScreen(navController = navController, vehicleId = vehicleId)
+        composable<Route.ImageUpload> { backStackEntry ->
+            val args = backStackEntry.toRoute<Route.ImageUpload>()
+            ImageUploadScreen(
+                navController = navController,
+                vehicleId = args.vehicleId
+            )
         }
-
-
-
-
-            }
     }
+}
 
 
