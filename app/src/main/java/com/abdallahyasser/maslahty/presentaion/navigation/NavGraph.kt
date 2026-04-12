e831f30345a00086108274ea45701448628dcfc6
MohsenAbdo22 <mohsenabdo213@gmail.com>
2026-04-12 17:10:00 +0200

    Refactor NavGraph.kt to streamline imports and enhance readability

--- app/src/main/java/com/abdallahyasser/maslahty/presentaion/navigation/NavGraph.kt
+++ app/src/main/java/com/abdallahyasser/maslahty/presentaion/navigation/NavGraph.kt
@@ -5,14 +5,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import com.abdallahyasser.maslahty.presentaion.navigation.Route
 import com.abdallahyasser.maslahty.presentaion.screens.auth.LoginScreen
 import com.abdallahyasser.maslahty.presentaion.screens.auth.OTPVerificationScreen
 import com.abdallahyasser.maslahty.presentaion.screens.auth.RegistrationScreen
-import com.abdallahyasser.maslahty.presentaion.screens.home.HomeScreen
 import com.abdallahyasser.maslahty.presentaion.screens.splash.SplashScreen
-import com.abdallahyasser.maslahty.presentaion.screens.transfer.imageUpload.ImageUploadScreen
-import com.abdallahyasser.maslahty.presentaion.screens.transfer.vehicleDetails.VehicleDetailsScreen
 import com.abdallahyasser.maslahty.presentaion.screens.transfer.imageUpload.ImageUploadScreen
 import com.abdallahyasser.maslahty.presentaion.screens.transfer.vehicleDetails.VehicleDetailsScreen
 import com.example.maslahty.presentation.screens.home.HomeScreen
