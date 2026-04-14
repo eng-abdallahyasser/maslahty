8914e630bf34adc6eee6ab79d49864a1012301e6
eng-abdallahyasser <abdallahyasser1277@gmail.com>
2026-04-14 17:18:53 +0200

    Add Hilt application class and network module for dependency injection

---
+++ app/src/main/java/com/abdallahyasser/maslahty/MaslahtyApplication.kt
@@ -0,0 +1,8 @@
+package com.abdallahyasser.maslahty
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MaslahtyApplication: Application() {
+}
---
+++ app/src/main/java/com/abdallahyasser/maslahty/di/NetworkModule.kt
@@ -0,0 +1,14 @@
+package com.abdallahyasser.maslahty.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+
+}
